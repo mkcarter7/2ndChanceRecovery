92f02b76b7c97ae1ed3d52473d15d7a779bda96c
Melissa Carter <mkd.princess@gmail.com>
2026-01-31 12:25:38 -0600

    Fix Django admin 500: add APP_DIRS to TEMPLATES

--- backend/recovery_center/settings.py
+++ backend/recovery_center/settings.py
@@ -66,6 +66,7 @@ ROOT_URLCONF = 'recovery_center.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
